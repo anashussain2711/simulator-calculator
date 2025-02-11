3baca1d7663be549d5b516ff45add73fcc6ba3ba
Anas <anashussain2711@gmail.com>
2025-02-11 20:04:01 +0500

    a
    a

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {}
 
 const isProd = process.env.NODE_ENV === 'production';
 
+if (typeof performance === 'undefined') {
+  global.performance = require('perf_hooks').performance;
+}
+
 module.exports = {
   assetPrefix: isProd ? '/simulator-calculator/' : '',
   basePath: isProd ? '/simulator-calculator' : '',
